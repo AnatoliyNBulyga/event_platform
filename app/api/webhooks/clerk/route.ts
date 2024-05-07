a8fbcaa7054718a65a1edf5afbb2416b9ab3ccd0
anatoliy <anatoliynbulyga@gmail.com>
2024-05-07 15:39:02 +0300

    fix route

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -59,9 +59,9 @@ export async function POST(req: Request) {
         const user = {
             clerkId: id,
             email: email_addresses[0].email_address,
-            username: username!,
-            firstName: first_name!,
-            lastName: last_name!,
+            username: username || 'Default username',
+            firstName: first_name || 'Default firstName',
+            lastName: last_name || 'Default lastName',
             photo: image_url
         }
 
